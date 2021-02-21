e1147c819e9acd7690549ccbd329612d49f451cf
vladikkuzn <vladikkuzn1@gmail.com>
2021-02-21 20:52:15 +0200

    Test for actitiry added

--- api/tests/test_activity.py
+++ api/tests/test_activity.py
@@ -3,6 +3,7 @@ from model_bakery import baker
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 from api.models import User, Post, Like
+import datetime
 
 
 class LikeTests(APITestCase):
@@ -15,22 +16,20 @@ class LikeTests(APITestCase):
             'username': self.username,
             'password': self.password
         }
-        self.like = baker.make('Like')
 
 
     def authorize_client(self):
         url = reverse('api_login')
         response = self.client.post(url, self.data, format='json')
-        self.client.post('api-auth/login/', self.data, format='json')
         self.client = APIClient()
         token = response.data['access']
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
 
-    
-    def test_analytics(self):
+
+    def test_activity(self):
         self.authorize_client()
-        url = reverse('api_analytics') 
-        response = self.client.get(url, {'date_from': '2021-01-01', 'date_to': '2021-12-01'}, format='json')
+        url = reverse('api_activity', kwargs={'pk': self.user.pk})
+        response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data['data']), 1)
-        self.assertEqual(response.data['data'][0]['liked_count'], 1)
+        self.assertEqual(response.data['last_login'], None)
+        self.assertEqual(response.data['last_request'][:10], str(datetime.datetime.today())[:10])
